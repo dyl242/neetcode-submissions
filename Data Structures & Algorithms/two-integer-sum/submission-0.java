05cac2d6d9e5ce1c52f8c5428ed50b2ac86ac45d
Dylan <dylan.haskell904@gmail.com>
2026-05-12 15:12:52 +0930

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // Check if the complement already exists
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            // Store current number and its index
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
